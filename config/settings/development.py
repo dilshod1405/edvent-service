8eb0d38f3a295664fd1496c2eb577df07d5969c4
Dilshod <dilshod.normurodov1392@gmail.com>
2025-05-03 19:34:34 +0900

    updated staticfiles in nginx

--- config/settings/development.py
+++ config/settings/development.py
@@ -23,3 +23,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'config/staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
