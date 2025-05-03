5da11b3ae674ad6324058b5dbdf36eb6c0b6533c
Dilshod <dilshod.normurodov1392@gmail.com>
2025-05-03 20:20:24 +0900

    updated staticfiles in nginx

--- authentication/Views/login_view.py
+++ authentication/Views/login_view.py
@@ -17,13 +17,9 @@ class LoginView(APIView):
         return Response({
             'refresh': str(refresh),
             'access': str(refresh.access_token),
-            'user': {
-                'id': user.id,
-                'username': user.username,
-                'email': user.email,
-                'first_name': user.first_name,
-                'last_name': user.last_name,
-                'role': user.role,  # if applicable
-                'photo': request.build_absolute_uri(user.photo.url) if user.photo else None,
-            }
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'role': user.role,
+            'photo': user.photo.url
         }, status=status.HTTP_200_OK)
