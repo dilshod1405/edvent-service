4ae7fc14eb58badfdda2c4fe956e93601d90f01b
Dilshod <dilshod.normurodov1392@gmail.com>
2025-05-04 14:00:02 +0900

    updated settings

--- config/settings/development.py
+++ config/settings/development.py
@@ -8,13 +8,3 @@ DEBUG = False
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ") + ["localhost", "127.0.0.1"]
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT"),
-    }
-}
--- config/settings/production.py
+++ config/settings/production.py
@@ -5,14 +5,3 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 DEBUG = True
 ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT"),
-    }
-}
