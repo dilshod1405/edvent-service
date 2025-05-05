10cef736be0e08d78a8e46779944b11cc6d706d5
Dilshod <dilshod.normurodov1392@gmail.com>
2025-05-05 12:44:44 +0900

    used ses

--- authentication/tasks.py
+++ authentication/tasks.py
@@ -1,23 +1,39 @@
 from celery import shared_task
 from django.core.mail import send_mail
+from django.conf import settings
+
+VERIFIED_FROM_EMAIL = 'www.edvent.uz@gmail.com'
 
 @shared_task
 def send_activation_email(email, username, activation_link):
+    subject = 'Edvent.uz - Profilingizni aktivlashtiring'
+    message = (
+        f"Assalomu alaykum {username},\n\n"
+        f"Edvent.uz saytida profilingizni aktivlashtirish uchun quyidagi havolaga bosing:\n"
+        f"{activation_link}\n\n"
+        f"Rahmat!"
+    )
     send_mail(
-        subject='Edvent.uz - Aktivlashtirish',
-        message=f'Assalomu aleykum. Edvent.uz saytida foydalanuvchi {username}, profilingizni aktivlashtirish uchun quyidagi havolaga bosing: {activation_link}',
-        from_email='www.edvent.uz@gmail.com',
+        subject=subject,
+        message=message,
+        from_email=VERIFIED_FROM_EMAIL,
         recipient_list=[email],
         fail_silently=False,
     )
 
-
 @shared_task
 def successful_registration_email(email, username):
+    subject = 'Edvent.uz - Muvaffaqiyatli ro‘yxatdan o‘tganingiz bilan!'
+    message = (
+        f"Assalomu alaykum {username},\n\n"
+        f"Tabriklaymiz! Siz Edvent.uz saytida muvaffaqiyatli ro‘yxatdan o‘tdingiz.\n"
+        f"Endi siz platformamizdan to‘liq foydalanishingiz mumkin.\n\n"
+        f"Omad tilaymiz!"
+    )
     send_mail(
-        subject='Edvent.uz - Tabriklaymiz !',
-        message=f'Assalomu aleykum. Edvent.uz saytida foydalanuvchi {username} muvaffaqiyatli ro\'yxatdan o\'tdi.',
-        from_email='www.edvent.uz@gmail.com',
+        subject=subject,
+        message=message,
+        from_email=VERIFIED_FROM_EMAIL,
         recipient_list=[email],
         fail_silently=False,
     )
