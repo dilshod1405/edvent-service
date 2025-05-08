5722f30296bde8c568bc96e1b9af7b5aef944a5b
Dilshod <dilshod.normurodov1392@gmail.com>
2025-05-08 21:24:49 +0900

    added logo img

--- config/settings/base.py
+++ config/settings/base.py
@@ -132,6 +132,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / "config" / "static"]
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
