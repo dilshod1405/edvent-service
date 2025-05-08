ab53f234371844f90b32b73e59997f1f5d88c9d1
Dilshod <dilshod.normurodov1392@gmail.com>
2025-05-08 21:16:35 +0900

    added Amazon SES service

--- config/settings/base.py
+++ config/settings/base.py
@@ -134,7 +134,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = '/media/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
