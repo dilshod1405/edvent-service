a5ba5524faec094f68f3ea2b90cecff61c27dc6c
Dilshod <dilshod.normurodov1392@gmail.com>
2025-05-10 12:13:40 +0900

    email logo updated

--- authentication/tasks/send_activation_email.py
+++ authentication/tasks/send_activation_email.py
@@ -12,7 +12,7 @@ def send_activation_email(email, username, first_name, last_name, activation_lin
     subject = 'Edvent.uz - Profilingizni aktivlashtiring'
     body_text = ""
 
-    logo_url = "https://i.imgur.com/X9JjKcx.png"
+    logo_url = "https://i.imgur.com/rRc8vsB.png"
 
     body_html = f"""
     <html>
