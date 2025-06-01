cb6c09fe183cd01bb0d5fbcc6076f3d151dea559
Dilshod <dilshod.normurodov1392@gmail.com>
2025-06-01 21:33:04 +0900

    added swagger

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,24 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from authentication.permissions import IsAdminUserOnly
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="archedu.uz (edvent.uz) API Documentation",
+        default_version='v1',
+        description="API documentation for the Archedu platform, formerly known as Edvent.",
+    ),
+    public=False,
+    permission_classes=[IsAdminUserOnly],
+)
+
 
 urlpatterns = [
     path('supercontroller/', admin.site.urls),
@@ -16,6 +30,7 @@ urlpatterns = [
     path('auth-token/', include('djoser.urls.authtoken')),  # Token-based auth routes
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
