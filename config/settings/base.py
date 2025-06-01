e0bbbf707f926e49a6f01f56e17ba784bba93b66
Dilshod <dilshod.normurodov1392@gmail.com>
2025-06-01 21:55:30 +0900

    added spectacular

--- config/settings/base.py
+++ config/settings/base.py
@@ -150,6 +150,7 @@ ADMIN_INTERFACE_ENABLED = True
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        'rest_framework.authentication.SessionAuthentication',
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
