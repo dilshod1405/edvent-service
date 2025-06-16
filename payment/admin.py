6caca69807a7286712fb8f61398ccc5eee311faa
Dilshod <dilshod.normurodov1392@gmail.com>
2025-06-16 21:56:08 +0900

    updated payment

--- payment/admin.py
+++ payment/admin.py
@@ -3,7 +3,7 @@ from .models import Transaction
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'course', 'module', 'tariff', 'amount', 'payme_transaction_id', 'state', 'payment_type', 'created_at', 'updated_at')
+    list_display = ('user', 'course', 'module', 'tariff', 'amount', 'payme_transaction_id', 'state', 'payment_type', 'created_at', 'updated_at', 'id')
     search_fields = ('user', 'course', 'module', 'tariff', 'amount', 'payme_transaction_id', 'state', 'payment_type', 'created_at', 'updated_at')
     ordering = ('-created_at', '-updated_at', '-id')
     list_filter = ('state', 'payment_type', 'created_at', 'updated_at')
